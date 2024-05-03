30bad9e29025ef4fdada6469ce19f11b82e4955e
chamber0x0 <160602770+chamber0x0@users.noreply.github.com>
2024-05-03 17:18:24 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,9 +1,13 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import time
 import random
+import warnings
 
+# Suppress BeautifulSoup warning
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
 BOLD = '\033[1m'
 YELLOW = '\033[93m'
@@ -11,8 +15,9 @@ CYAN = '\033[96m'
 RESET = '\033[0m'
 GREEN = '\033[32m'
 BLUE = '\033[34m'
+RED = '\033[31m'
 
-def crawl_website(url, depth=1):
+def crawl_website(url, depth=1, proxy=None):
     if depth < 0:
         return
 
@@ -29,9 +34,12 @@ def crawl_website(url, depth=1):
             continue
 
         try:
-            response = requests.get(url)
+            if proxy:
+                response = requests.get(url, proxies={"http": proxy, "https": proxy})
+            else:
+                response = requests.get(url)
         except requests.exceptions.RequestException as e:
-            print(e)
+            print(RED + str(e) + RESET)
             continue
 
         if response.status_code == 200:
@@ -47,40 +55,31 @@ def crawl_website(url, depth=1):
                 if complete_url_domain == domain:
                     to_crawl.append(complete_url)
 
-            print(url)
+            print(GREEN + url + RESET)
 
             crawled.add(url)
-        
-    #   if the site has rate limiting then remove the comment otherwise leave it as it is for faster results.
-    #        time.sleep(random.uniform(1, 2))  
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simple website crawler.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", "--url", help="URL of the website to crawl.")
+    group.add_argument("-l", "--list", help="Path to the file containing list of URLs to crawl.")
+    parser.add_argument("--proxy", help="Proxy address to use for the requests.")
+    args = parser.parse_args()
 
-print('')
-print('')
-print(RESET + "-"*130)
-target = input(BOLD + YELLOW + "Enter The Target You Want To Crawl: " + CYAN)
-print(RESET + "-"*130)
-
-print('')
-print('')
-
-print(BOLD + YELLOW + "Target: " + BOLD + CYAN + target + RESET)
-
-print('')
-print('')
-
-print(BOLD + BLUE + """[+] Starting [+]
-      
-      """ + RESET + GREEN)
-
-if not target.startswith('http'):
-    target = 'https://' + target
-
-crawl_website(target, depth=2)
-
-print(BOLD + BLUE + """ 
-      
-[+] Finished [+]
-      
-      """)
-
+    if args.url:
+        print(BOLD + YELLOW + "\n\nTarget: " + BOLD + CYAN + args.url + RESET)
+        print(BOLD + BLUE + "\n\n[+] Starting [+]\n\n" + RESET + GREEN)
+        if not args.url.startswith('http'):
+            args.url = 'https://' + args.url
+        crawl_website(args.url, depth=2, proxy=args.proxy)
+    elif args.list:
+        with open(args.list, 'r') as f:
+            urls = f.read().splitlines()
+            for url in urls:
+                print(BOLD + YELLOW + "\n\nTarget: " + BOLD + CYAN + url + RESET)
+                print(BOLD + BLUE + "\n\n[+] Starting [+]\n\n" + RESET + GREEN)
+                if not url.startswith('http'):
+                    url = 'https://' + url
+                crawl_website(url, depth=2, proxy=args.proxy)
+    print(BOLD + BLUE + "\n\n[+] Finished [+]\n\n" + RESET)
